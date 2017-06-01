42a0572edbfdeb2c64ab837695f2dcb047469402
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-01 16:41:27 -0400

    Create favourites page w links from array on server, strip links using File.basename more attractive, -works

--- server.rb
+++ server.rb
@@ -20,3 +20,11 @@ get '/about_me' do
 erb :about_me
 
 end
+
+get '/favourites' do
+
+@fav_links = ["http://www.dailyxtra.com/", "http://themenace.transcommunity.ca/", "http://www.ic.org/", "https://twitter.com/MykkiBlanco"]
+
+erb :favourites
+
+end
